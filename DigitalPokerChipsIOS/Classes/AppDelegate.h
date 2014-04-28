71aaf55da010442c356101e175cc06cfe2d4b2a4
poliver87 <peter.oliver@bidjee.com>
2014-04-29 00:00:21 +1000

    Initial Commit

---
+++ DigitalPokerChipsIOS/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  DigitalPokerChipsIOS
+//
+//  Created by Peter Oliver on 29/04/2014.
+//  Copyright Bidjee 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
